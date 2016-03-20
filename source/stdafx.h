d1490277919b2fe170f5ddb8ab7c5ccd86bbddd7
Morten <samsogade@gmail.com>
2016-03-20 13:22:23 +0800

    Updated working example

--- source/stdafx.h
+++ source/stdafx.h
@@ -5,42 +5,20 @@
 
 #pragma once
 
-// Modify the following defines if you have to target a platform prior to the ones specified below.
-// Refer to MSDN for the latest info on corresponding values for different platforms.
+#include <string>
+#include <iostream>
 
-#ifdef WIN32
-#   ifndef WINVER                    // Allow use of features specific to Windows XP or later.
-#       define WINVER 0x0501         // Change this to the appropriate value to target other versions of Windows.
-#   endif
-#   ifndef _WIN32_WINNT              // Allow use of features specific to Windows XP or later.                   
-#       define _WIN32_WINNT 0x0501   // Change this to the appropriate value to target other versions of Windows.
-#   endif                        
-#   ifndef _WIN32_WINDOWS            // Allow use of features specific to Windows 98 or later.
-#       define _WIN32_WINDOWS 0x0410 // Change this to the appropriate value to target Windows Me or later.
-#   endif
-#   ifndef _WIN32_IE                 // Allow use of features specific to IE 6.0 or later.
-#       define _WIN32_IE 0x0600      // Change this to the appropriate value to target other versions of IE.
-#   endif
-#   ifndef PTHREADS
-#       define WIN32_LEAN_AND_MEAN     // Exclude rarely-used stuff from Windows headers
-#       include <windows.h>
-#   endif
-#else
-#   define PTHREADS
-#   define PTHREADS_MUTEX
-#endif
+#include <ForexConnect.h>
+#include <sample_tools.h>
 
-#ifdef WIN32
-#   define GSTOOL3 __declspec(dllexport)
-#else
-    #define GSTOOL3
+#include <list>
+#include <map>
+#include <vector>
+#include <ctime>
+#include <cstring>
+#include <cmath>
 
-    #include <sys/types.h>
-    #include <sys/stat.h>
-    #include <unistd.h>
-    #include <errno.h>
-    #include <dirent.h>
-#endif
-
+#define _TIMEOUT 30000
 
 // TODO: reference additional headers your program requires here
+
